a42cbc01706bbec6583d7e77fec4d30a1aa2c2e5
Ralph <ralph@Ralphs-MacBook-Pro.local>
2013-11-17 01:19:56 +0800

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,8 +25,11 @@ class ApplicationController < ActionController::Base
     message = params['message_payload']['message'].downcase.gsub('reply ','')
     msg = params['message_payload']['message'].split(' ')[0].downcase
     if msg.eql?('reply')
-      x = Net::HTTP.post_form(URI.parse("tma.herokuapp.com/notify?msg_id=2&message=#{message}"))
-      puts x.body   
+      Credential.all.each do |sub|
+      uri = URI.parse("http://devapi.globelabs.com.ph/smsmessaging/v1/outbound/5385/requests")
+      uri.query="app_id=9dL6AsryyoGhRdcAq4iyMXhR9LnbsBaK&app_secret=8c7fd2453adb5859b3caf49d3a76f0662807dcb462a668f742feebe9fc4408e8&passphrase=globelabsawesome"
+      response = Net::HTTP.post_form(uri, {"address" => sub.subscriber, "message" => "#{params[:msg_id]} #{params[:message]}"})
+      end
     end
   end
 end
