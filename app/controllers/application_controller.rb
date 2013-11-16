1f6a3153489f41682abe23a7ae4f933f0ed91902
Jesse Panganiban <me@jpanganiban.com>
2013-11-17 00:38:50 +0800

    Logs and passphrase

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,10 @@ class ApplicationController < ActionController::Base
   def feed
     Credential.all.each do |sub|
       uri = URI.parse("http://devapi.globelabs.com.ph/smsmessaging/v1/outbound/5385/requests")
-      uri.query="app_id=9dL6AsryyoGhRdcAq4iyMXhR9LnbsBaK&app_secret=8c7fd2453adb5859b3caf49d3a76f0662807dcb462a668f742feebe9fc4408e8"  
+      uri.query="app_id=9dL6AsryyoGhRdcAq4iyMXhR9LnbsBaK&app_secret=8c7fd2453adb5859b3caf49d3a76f0662807dcb462a668f742feebe9fc4408e8&passphrase=globelabsawesome"
       response = Net::HTTP.post_form(uri, {"address" => sub.subscriber, "message" => "#{params[:msg_id]} #{params[:message]}"})
+      puts response
+      puts "SENDING SMS TO: #{sub.subscriber}"
 		end
 
     render json: {success: true}
