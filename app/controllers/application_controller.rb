42888895051e886171c00bce0d6c050f9de460c3
Jesse Panganiban <me@jpanganiban.com>
2013-11-17 00:34:30 +0800

    Require net/http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
   require 'net/http'
@@ -9,11 +11,9 @@ class ApplicationController < ActionController::Base
 
   def feed
     Credential.all.each do |sub|
-      puts "hello"
       uri = URI.parse("http://devapi.globelabs.com.ph/smsmessaging/v1/outbound/5385/requests")
       uri.query="app_id=9dL6AsryyoGhRdcAq4iyMXhR9LnbsBaK&app_secret=8c7fd2453adb5859b3caf49d3a76f0662807dcb462a668f742feebe9fc4408e8"  
       response = Net::HTTP.post_form(uri, {"address" => sub.subscriber, "message" => "#{params[:msg_id]} #{params[:message]}"})
-      puts response
 		end
 
     render json: {success: true}
