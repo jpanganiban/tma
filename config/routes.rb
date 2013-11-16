72d62e89518e8df07ab8d6f15338fdaccdd871fb
Ralph <ralph@Ralphs-MacBook-Pro.local>
2013-11-16 23:40:46 +0800

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 YolandaStream::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  match "callback" => "notify#callback"
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
